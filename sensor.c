4845bf6f284471a57c81e02f812e65fdd027595f
David Howlett <david.howlett@c2mdesign.co.uk>
2015-04-16 15:45:56 +0000

    hopefully the last edit ever

--- sensor.c
+++ sensor.c
@@ -9,26 +9,33 @@ int main(){
     int file;
     int adapter_nr = 1; /* probably dynamically determined */
     char filename[20];
-    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+    puts("This is a pressure sensor");
+    sprintf(filename, "/dev/i2c-%d", adapter_nr);
     file = open(filename, O_RDWR);
     if (file < 0) {
         /* ERROR HANDLING; you can check errno to see what went wrong */
-        puts("file opened incorrectly");
+        puts("File opened incorrectly");
         exit(1);
     }
     int addr = 120; /* The I2C address */
 
     if (ioctl(file, I2C_SLAVE, addr) < 0) {
         /* ERROR HANDLING; you can check errno to see what went wrong */
-        puts("could not set address of the sensor");
+        puts("Could not set address of the sensor");
         exit(1);
     }
-    char buf[10];
+    char buf[4];
+    buf[2]=255;
+    buf[3]=0;
+    //int i;
+    //for(i=0;i<100000;i++){
     while(1){
-        read(file, buf, 6);
-        //printf("%d\n",buf[0]);
-        //printf("%d\n",buf[1]);
-        printf("%d\n",256*buf[0]+buf[1]);
-        usleep(10000);
+        read(file, buf, 2);
+        printf(buf);
+        
+        
+    
+        //printf("%d\n",256*buf[0]+buf[1]);
+        //usleep(10000);
     }
 }
